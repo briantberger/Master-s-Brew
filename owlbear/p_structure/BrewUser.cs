5c077ad798bdac4cfeb6e6f7d16959519e582dc6
briantberger <brianberger23@gmail.com>
2023-05-03 11:22:32 -0700

    started page for constructing new items

--- owlbear/p_structure/BrewUser.cs
+++ owlbear/p_structure/BrewUser.cs
@@ -1,11 +1,30 @@
-﻿{
-  "ConnectionStrings": {
-    "DefaultConnection": "DataSource=app.db"
-  },
-  "Logging": {
-    "LogLevel": {
-      "Default": "Warning"
+﻿using System;
+using Items;
+
+namespace Structure
+{
+    public class BrewUser
+    {
+        private List<CharacterClass> m_userClasses;
+        private List<PlayerCharacter> m_userCharacters;
+        private List<BasicItem> m_userItems;
+
+        public BrewUser()
+        {
+            m_userClasses = new List<CharacterClass>();
+            m_userCharacters = new List<PlayerCharacter>();
+            m_userItems = new List<BasicItem>();
+        }
+
+        public BrewUser(List<CharacterClass> classes,
+                        List<PlayerCharacter> characters,
+                        List<BasicItem> items)
+        {
+            m_userClasses = classes;
+            m_userCharacters = characters;
+            m_userItems = items;
+        }
+
+
     }
-  },
-  "AllowedHosts": "*"
 }
