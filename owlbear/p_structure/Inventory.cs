d5e3e48d9d8d6b10c87eda2e3b4e30928336668b
briantberger <brianberger23@gmail.com>
2023-05-10 19:15:36 -0700

    basic inventory layout for PC

--- owlbear/p_structure/Inventory.cs
+++ owlbear/p_structure/Inventory.cs
@@ -163,34 +163,6 @@ namespace Structure
             // TODO: actually define the Exception
             throw new Exception();
         }
-
-        /// <summary>
-        /// Represents a single inventory slot, consisting of the item in that
-        /// slot and the quantity of item.
-        /// </summary>
-        public class InventorySlot
-        {
-            private BasicItem m_item;
-            private int m_quantity;
-
-            public InventorySlot(BasicItem item, int quantity)
-            {
-                this.m_item = item;
-                this.m_quantity = quantity;
-            }
-
-            public BasicItem Item { get => m_item; set => m_item = value; }
-            public int Quantity { get => m_quantity; set => m_quantity = value; }
-
-            public void IncQuantity()
-            {
-                this.m_quantity++;
-            }
-
-            public void DecQuantity()
-            {
-                this.m_quantity--;
-            }
-        }
     }
 }
+
---
+++ owlbear/p_structure/InventorySlot.cs
@@ -0,0 +1,33 @@
+﻿using Items;
+
+namespace Structure
+{
+    public class InventorySlot
+    {
+        private BasicItem m_item;
+        private int m_quantity;
+
+        public InventorySlot(BasicItem item, int quantity)
+        {
+            this.m_item = item;
+            this.m_quantity = quantity;
+        }
+
+        public BasicItem Item { get => m_item; set => m_item = value; }
+        public int Quantity { get => m_quantity; set => m_quantity = value; }
+
+        public void IncQuantity()
+        {
+            this.m_quantity++;
+        }
+
+        public void DecQuantity()
+        {
+            this.m_quantity--;
+        }
+    }
+}
+/// <summary>
+/// Represents a single inventory slot, consisting of the item in that
+/// slot and the quantity of item.
+/// </summary>
